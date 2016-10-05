014e3da80bc01a44c436fa20de8c106118ad6a33
claupaos <coviedo11@javerianacali.edu.co>
2016-10-04 19:44:18 -0500

    Primer Archivo

---
+++ ejemplo.py
@@ -0,0 +1,14 @@
+#Introduccion a la programacion
+#Ejemplo Algoritmo
+
+def es_numero_positivo(numero):
+
+    if numero > 0:
+        print ("el numero es positivo")
+    else:
+        print ("el numero es negativo")
+
+print("ejemplos")        
+es_numero_positivo(4)
+es_numero_positivo(-10)
+    
